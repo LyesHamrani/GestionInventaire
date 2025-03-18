f3640e86f87da643675d84a9dabd63436d3ecbdc
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2025-03-17 23:55:20 -0400

    Ajoutez des fichiers projet.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GestionInventaire.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+        public ViewModelBase CurrentChildView
+        {
+            get => _currentChildView;
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView)); 
+            }
+        }
+
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public IconChar Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        public ICommand ShowHomeViewCommand { get; }
+
+        public MainViewModel()
+        {
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+
+            ExecuteShowHomeViewCommand(null);
+
+        }
+
+        private void ExecuteShowHomeViewCommand (object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Accueil";
+            Icon = IconChar.Home;
+        }
+    }
+}
